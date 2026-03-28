b469eaed933c6f5beabe030aec30b4ccc1fdbd21
Ines-Sekfali <ines.sekfali@ensia.edu.dz>
2026-03-29 00:09:28 +0100

    feat :  create database.py file and requerement.txt

---
+++ backend/database.py
@@ -0,0 +1,35 @@
+from sqlalchemy import create_engine, Column, Integer, String, Boolean, DateTime
+from sqlalchemy.orm import declarative_base, sessionmaker
+from datetime import datetime, timedelta
+
+DATABASE_URL = "sqlite:///./tickets.db"
+
+engine = create_engine(DATABASE_URL)
+SessionLocal = sessionmaker(bind=engine)
+Base = declarative_base()
+
+class Ticket(Base):
+    __tablename__ = "tickets"
+
+    id = Column(Integer, primary_key=True, index=True)
+    text = Column(String)
+    author = Column(String)
+    platform = Column(String)
+    fb_link = Column(String)
+    created_at = Column(DateTime)
+
+    category = Column(String)
+    category_manual = Column(String, nullable=True)
+    manually_corrected = Column(Boolean, default=False)
+
+    sentiment = Column(String)
+    is_urgent = Column(Boolean)
+
+    topic = Column(String)
+
+    sla_deadline = Column(DateTime)
+    status = Column(String)
+
+    fetched_at = Column(DateTime, default=datetime.utcnow)
+
+Base.metadata.create_all(bind=engine)
