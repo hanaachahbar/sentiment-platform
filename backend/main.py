98cc6e8662640266729d4795443a904a11ae1af6
Ines-Sekfali <ines.sekfali@ensia.edu.dz>
2026-03-29 21:56:29 +0100

    Update main.py

--- backend/main.py
+++ backend/main.py
@@ -1,5 +1,5 @@
 from fastapi import FastAPI, Query
-from routers import posts, stats, tickets, export
+from routers import posts, stats, tickets, export, trends
 
 app = FastAPI()
 
@@ -8,3 +8,4 @@ app.include_router(posts.router)
 app.include_router(stats.router)
 app.include_router(tickets.router)
 app.include_router(export.router)
+app.include_router(trends.router)
